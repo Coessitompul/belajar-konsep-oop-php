6534622fca634ff412ea54e6ae486c61c4fd87b5
Coes Sitompul <coessitompul99@gmail.com>
2020-10-01 17:09:36 +0700

    Video 3 : Metode set dan get

--- index.php
+++ index.php
@@ -4,25 +4,47 @@
   class robot {
     
     // property
-    public $suara = "ngik ngik";
-    public $berat = 30;
+    public $suara;
+    public $berat;
 
-    // metode
-    public function bersuara() {
-      echo "Suara robot " . $this->suara;
+    // metode set && get
+    public function setSuara($suara) {
+      $this->suara = $suara;
     }
 
-    public function beratRobot() {
+    public function getSuara() {
+      return $this->suara;
+    }
+
+    public function setBerat($berat) {
+      $this->berat = $berat;
+    }
+
+    public function getBerat() {
       return $this->berat;
-      // return disini bertujuan untuk mengembalikan nilai dari nilai berat dari property berat ketika fungsi ini dipanggil
     }
 
   }
 
   $robot1 = new robot;
-  $robot1->bersuara();
-  echo $robot1->beratRobot();
+  $robot2 = new robot;
+  
+  // set digunakan untuk men-set suara 'ngik ngik ngok' ke dalam property suara yang ada di dalam class robot.
+  $robot1->setSuara('ngik ngik ngok');
+  // get digunakan untuk mengambil data dari dalam property yang berada didalam class robot, jadi ketika nilainya sudah di set maka akan langsung diambil nilainya, sesuai dengan program yang ditulis didalam function getSuara()
+  echo "Suara robotnya adalah " . $robot1->getSuara();
 
+  $robot2->setSuara('tereng tereng teng');
+  echo "Suara robotnya adalah " . $robot2->getSuara();
+
+  $robot1 = new robot;
+  $robot2 = new robot;
+    
+  $robot1->setBerat(50);
+  echo "Berat robotnya adalah " . $robot1->getBerat();
+
+  $robot2->setBerat(100);
+  echo "Berat robotnya adalah " . $robot2->getBerat();
 
 
 ?>
