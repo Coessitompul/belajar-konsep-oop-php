70490b237fe27e9fd68b5ac70aecfbad9166a85f
Coes Sitompul <coessitompul99@gmail.com>
2020-10-02 17:52:24 +0700

    Video 4: Mengenal Konstructor

--- index.php
+++ index.php
@@ -1,50 +1,25 @@
 <?php 
+  
+  require_once 'robot.php'; 
 
-  // class
-  class robot {
-    
-    // property
-    public $suara;
-    public $berat;
-
-    // metode set && get
-    public function setSuara($suara) {
-      $this->suara = $suara;
-    }
-
-    public function getSuara() {
-      return $this->suara;
-    }
-
-    public function setBerat($berat) {
-      $this->berat = $berat;
-    }
-
-    public function getBerat() {
-      return $this->berat;
-    }
-
-  }
-
-  $robot1 = new robot;
-  $robot2 = new robot;
+  $robot1 = new robot('ngik ngik ngok', 50);
+  $robot2 = new robot('ngik ngik ngok ngok', 100);
   
   // set digunakan untuk men-set suara 'ngik ngik ngok' ke dalam property suara yang ada di dalam class robot.
-  $robot1->setSuara('ngik ngik ngok');
-  $robot1->setSuara('ngik ngik ngok ngok');
+
   // get digunakan untuk mengambil data dari dalam property yang berada didalam class robot, jadi ketika nilainya sudah di set maka akan langsung diambil nilainya, sesuai dengan program yang ditulis didalam function getSuara()  
+  $robot1->setBerat(30);
+  echo "Suara robotnya adalah " . $robot1->getSuara() . " " . "dengan berat robot " . $robot1->getBerat() . "<br>";
+  echo "Suara robotnya adalah " . $robot2->getSuara() . " " . "dengan berat robot " . $robot2->getBerat() . "<br>";
 
-  $robot2->setSuara('tereng tereng teng');
-  echo "Suara robotnya adalah " . $robot2->getSuara();
-
-  $robot1 = new robot;
-  $robot2 = new robot;
+  // $robot1 = new robot(100);
+  // $robot2 = new robot(100);
     
-  $robot1->setBerat(50);
-  echo "Berat robotnya adalah " . $robot1->getBerat();
+  // $robot1->setBerat(50);
+  // echo "Berat robotnya adalah " . $robot1->getBerat() . "<br>";
 
-  $robot2->setBerat(100);
-  echo "Berat robotnya adalah " . $robot2->getBerat();
+  // $robot2->setBerat(100);
+  // echo "Berat robotnya adalah " . $robot2->getBerat();
 
 
 ?>
