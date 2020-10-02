1b6441a309914a29a90738a5308536fd5b5946a0
Coes Sitompul <coessitompul99@gmail.com>
2020-10-02 17:51:47 +0700

    Video 4 : Mengenal Konstructor

---
+++ robot.php
@@ -0,0 +1,37 @@
+<?php 
+
+
+  // class
+  class robot {
+    
+    // property
+    public $suara;
+    public $berat;
+
+    // constructor
+    public function __construct($suara, $berat) {
+      $this->suara = $suara;
+      $this->berat = $berat;
+    }    
+
+    // metode set && get
+    public function setSuara($suara) {
+      $this->suara = $suara;
+    }
+
+    public function getSuara() {
+      return $this->suara;
+    }
+
+    public function setBerat($berat) {
+      $this->berat = $berat;
+    }
+
+    public function getBerat() {
+      return $this->berat;
+    }
+
+  }
+
+
+?>
