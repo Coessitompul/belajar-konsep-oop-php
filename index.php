5922dfbabdccb1dbb0f8a55d111de0e6939a02e2
Coes Sitompul <coessitompul99@gmail.com>
2020-10-04 21:11:49 +0700

    Video 13: Autoloading

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php 
   
-  require_once 'robot.php'; 
-  require_once 'hewan.php';
-  require_once 'orang.php';
+  // require_once 'robot.php'; 
+  // require_once 'hewan.php';
+  // require_once 'orang.php';
+
+// autoload class ini adalah cara mudah supaya tidakk perlu lagi menggunakan require_once() secara berulang-ulang ketika memanggil class.
+spl_autoload_register(function ($class_name) {
+  include "foldername/" . $class_name . '.php';
+});
 
   $robot1 = new robot('testing', 10);
-  // penggunaan method chaining, penggunaan method chaining mengharuskan kita membuat fungsi yang harus mereturn this pada fungsinya. contoh dalam hal ini adalah fugsi setSuara(). jadi setSuara() harus mengembalikan nilai $this pada fungsinya, itu bertujuan supaya method yang disambungkan di belakangnya tidak mempunyai masalah, 
-  // supaya lebih jelas, return $this digunakan, supaya ketika fungsi itu dipanggil, maka dia mengembalikan nilai thisnya atau dalam conteks ini adalah robot, sehingga di setBerat() nilai yang sudah dipakai adalah robot. jika tidak demikian maka program akan error.
-  // setiap method yang akan dibelakangnya akan di chaining maka harus mengembalikan nilai this, atau mereturn nilai $this.
+  
   $robot1->setSuara('hoak hoak')->setBerat(50);  
 
   echo $robot1->getSuara() . " - " . $robot1->getBerat();
