afbbe5dfa4caf1515aee92b677da82ac2cdf4072
Coes Sitompul <coessitompul99@gmail.com>
2020-10-06 11:20:02 +0700

    Video 13: Cara membuat autoloading

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
   // require_once 'orang.php';
 
 // autoload class ini adalah cara mudah supaya tidakk perlu lagi menggunakan require_once() secara berulang-ulang ketika memanggil class.
+// spl_autoload_register(function ($class_name) {
+//   include "foldername/" . $class_name . '.php';
+// });
+
+// dibawah ini kita tidak perlu menggunakan "foldername/" karena semua filenya ada didalam satu repo/folder yang sama
 spl_autoload_register(function ($class_name) {
-  include "foldername/" . $class_name . '.php';
+  include $class_name . '.php';
 });
 
   $robot1 = new robot('testing', 10);
